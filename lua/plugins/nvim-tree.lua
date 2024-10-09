34cb535f96094b2de1e66420c5039f47312d00bd
Maksim Samoilov <maxim.samoilov@team.wrike.com>
2024-10-09 14:40:49 +0200

    Add config for nvim-tree

---
+++ lua/plugins/nvim-tree.lua
@@ -0,0 +1,46 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  lazy = false,
+  config = function ()
+    require("nvim-tree").setup({
+      root_dirs = {
+        "~/wrike/code"
+      },
+      git = {
+        enable = true,
+        timeout = 2000,
+      },
+      modified = {
+        enable = true,
+      },
+      filters = {
+        enable = true,
+        git_ignored = false,
+        custom = {
+          ".git",
+          ".pytest_cache",
+          "__pycache__"
+        }
+      },
+      live_filter = {
+        always_show_folders = false
+      },
+      filesystem_watchers = {
+        ignore_dirs = {
+          "__pycache__",
+          ".pytest_cache",
+          "build",
+        }
+      },
+      actions = {
+        expand_all = {
+          exclude = {
+            "__pycache__",
+            ".pytest_cache",
+            ".git"
+          }
+        }
+      }
+    })
+  end
+}
