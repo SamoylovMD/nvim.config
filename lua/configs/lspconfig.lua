9dc0c3f2764b7c21414e4e4d4f98505ac0f1d555
Maksim Samoilov <maxim.samoilov@team.wrike.com>
2024-10-30 14:28:12 +0100

    Add python lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,7 +4,7 @@ require("nvchad.configs.lspconfig").defaults()
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
+local servers = { "pyright" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
 -- lsps with default config
@@ -15,6 +15,22 @@ for _, lsp in ipairs(servers) do
     capabilities = nvlsp.capabilities,
   }
 end
+
+lspconfig.pyright.setup {
+  settings = {
+    pyright = {
+      autoImportCompletion = true
+    },
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        diagnosticMode = 'openFilesOnly',
+        useLibraryCodeForTypes = true,
+        typeCheckingMode = 'on'
+      }
+    }
+  }
+}
 
 -- configuring single server, example: typescript
 -- lspconfig.ts_ls.setup {
